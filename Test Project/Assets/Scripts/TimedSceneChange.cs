1b3f7851b3ba45f0e1d55c458d61e69f62fc17be
Kathryn Hite <Kathryn.Hite@students.olin.edu>
2015-11-14 14:58:30 -0500

    Added timed splash screen

---
+++ Test Project/Assets/Scripts/TimedSceneChange.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimedSceneChange : MonoBehaviour {
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(3f);
+        Application.LoadLevel("MiniGame");
+    }
+}
